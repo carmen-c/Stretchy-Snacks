d69da7c3d046c28d50c1287d3ccbd052457b4667
carmen-c <carmen.csmwc@gmail.com>
2016-12-08 11:11:39 -0800

    commit

--- Stretchy Snacks/ViewController.swift
+++ Stretchy Snacks/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // MARK: Buttons
+    @IBAction func plusButton(_ sender: UIButton) {
+        print ("plus icon pressed")
+        self.navHeightConstraint.constant = 200
+        UIView.animate(withDuration: 2.0, delay: 0, usingSpringWithDamping: <#T##CGFloat#>, initialSpringVelocity: <#T##CGFloat#>, options: <#T##UIViewAnimationOptions#>, animations: <#T##() -> Void#>, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+            
+            self.view.layoutIfNeeded()
+        })
+    }
+    
+    
+    
+    // MARK: Properties
+    @IBOutlet weak var navHeightConstraint: NSLayoutConstraint!
+    
+    
+    
+    // MARK: Views
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+     
     }
 
     override func didReceiveMemoryWarning() {
