fa9c3664f996a1dbcde7466def576a0cfba02c5b
carmen-c <carmen.csmwc@gmail.com>
2016-12-08 10:38:52 -0800

    Initial Commit

---
+++ Stretchy Snacks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Stretchy Snacks
+//
+//  Created by carmen cheng on 2016-12-08.
+//  Copyright © 2016 carmen cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
